2301e039ca93aab0d3886c7343797fca3ffb14d9
Sujeet Kumar <sujeetkv90@gmail.com>
2016-04-18 11:49:00 +0530

    Updated script

--- notify.js
+++ notify.js
@@ -1,5 +1,6 @@
 /**
  * notify - jQuery notification system
+ * v1.0
  * @author Sujeet <sujeetkv90@gmail.com>
  * @link https://github.com/sujeet-kumar/jquery-notify
  */
@@ -20,16 +21,22 @@
 			'error': '#F2DEDE'
 		};
 		
-		var _position = {
-			'ver': 'bottom',
-			'hor': 'right'
+		var _settings = {
+			'app_title': '',
+			'app_icon': '',
+			'position': {
+				'ver': 'bottom',
+				'hor': 'right'
+			},
+			'container_layout': '<div style="padding:5px; margin:0px; width:400px;"></div>',
+			'message_layout': '<div style="border-radius:8px; box-shadow:0 0 5px rgba(51, 51, 51, 0.4); font-weight:bold; padding:10px; margin:8px 5px;"></div>'
 		};
 		
-		var _system_icon = '';
-		
-		var _container_layout = '<div style="padding:5px; margin:0px; width:400px;"></div>';
-		
-		var _message_layout = '<div style="border-radius:8px; box-shadow:0 0 5px rgba(51, 51, 51, 0.4); font-weight:bold; padding:10px; margin:8px 5px;"></div>';
+		var _permissions = {
+			DEFAULT: 'default',
+			GRANTED: 'granted',
+			DENIED: 'denied'
+		};
 		
 		var _remove = function($node){
 			$node.fadeOut('fast', function(){
@@ -39,15 +46,15 @@
 		
 		var _container = function(){
 			if(!($('div#__notify_container').length)){
-				var $_container_layout = $(_container_layout);
+				var $_container_layout = $(_settings.container_layout);
 				$_container_layout.attr('id', '__notify_container');
 				
 				var _container_css = {
 					'z-index': '1024',
 					'position': 'fixed'
 				};
-				_container_css[_position.ver] = '0';
-				_container_css[_position.hor] = '0';
+				_container_css[_settings.position.ver] = '0';
+				_container_css[_settings.position.hor] = '0';
 				$_container_layout.css(_container_css);
 				
 				$('body').append($_container_layout);
@@ -66,7 +73,7 @@
 		var _template = function(type, msg, temp){
 			temp = (typeof temp == 'undefined') ? true : temp;
 			
-			var $tpl = $(_message_layout);
+			var $tpl = $(_settings.message_layout);
 			$tpl.html(msg);
 			$tpl.addClass('__notify_msg '+type);
 			$tpl.css({'color': _color[type], 'background-color': _bg_color[type], 'border': '1px solid '+_color[type]});
@@ -109,10 +116,10 @@
 		var _sys_alerts = [];
 		
 		var _notify_system = function(type, msg, temp){
-			var _title = 'Alert ' + type.charAt(0).toUpperCase() + type.substr(1);
+			var _title = $.trim(_settings.app_title + ' Alert: ' + type.charAt(0).toUpperCase() + type.substr(1));
 			
 			var _options = {body: msg};
-			if(_system_icon) _options.icon = _system_icon;
+			if(_settings.app_icon) _options.icon = _settings.app_icon;
 			
 			var _n = new Notification(_title, _options);
 			if(parseInt(temp) > 1){
@@ -123,11 +130,11 @@
 		
 		var _system_alert = function(type, msg, temp){
 			if('Notification' in window){
-				if(Notification.permission === 'granted'){
+				if(Notification.permission === _permissions.GRANTED){
 					_notify_system(type, msg, temp);
-				}else if(Notification.permission !== 'denied'){
+				}else if(Notification.permission !== _permissions.DENIED){
 					Notification.requestPermission(function(permission){
-						if(permission === 'granted'){
+						if(permission === _permissions.GRANTED){
 							_notify_system(type, msg, temp);
 						}else{
 							_default_api[type](msg, temp);
@@ -142,7 +149,7 @@
 		};
 		
 		var _system_alert_clear = function(){
-			if('Notification' in window && Notification.permission === 'granted'){
+			if('Notification' in window && Notification.permission === _permissions.GRANTED){
 				for(var _i in _sys_alerts){
 					_sys_alerts[_i].close();
 				}
@@ -170,11 +177,12 @@
 		};
 		
 		_default_api.setup = function(options){
-			if(options.container_layout) _container_layout = options.container_layout;
-			if(options.message_layout) _message_layout = options.message_layout;
-			if(options.system_icon) _system_icon = options.system_icon;
-			if(options.position && options.position.ver) _position.ver = options.position.ver;
-			if(options.position && options.position.hor) _position.hor = options.position.hor;
+			if(options.app_title) _settings.app_title = options.app_title;
+			if(options.app_icon) _settings.app_icon = options.app_icon;
+			if(options.container_layout) _settings.container_layout = options.container_layout;
+			if(options.message_layout) _settings.message_layout = options.message_layout;
+			if(options.position && options.position.ver) _settings.position.ver = options.position.ver;
+			if(options.position && options.position.hor) _settings.position.hor = options.position.hor;
 		};
 		
 		return _default_api;
